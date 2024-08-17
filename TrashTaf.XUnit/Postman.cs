cafd5191feb37cfc3368e5c07356e0656b103405
RF <rf@eggnine.com>
2024-08-17 01:32:07 -0400

    Added postman stub and trash context

---
+++ TrashTaf.XUnit/Postman.cs
@@ -0,0 +1,7 @@
+﻿// TrashTaf © 2024 by RF@EggNine.com All Rights Reserved
+namespace TrashTaf.XUnit
+{
+    public class Postman
+    {
+    }
+}
---
+++ TrashTaf.XUnit/TrashContext.cs
@@ -0,0 +1,11 @@
+﻿// TrashTaf © 2024 by RF@EggNine.com All Rights Reserved
+namespace TrashTaf.XUnit
+{
+    public class TrashContext
+    {
+        public Dictionary<string, object> Properties;
+        internal string TestName;
+        internal string ClassName;
+        internal int TestCaseId;
+    }
+}
