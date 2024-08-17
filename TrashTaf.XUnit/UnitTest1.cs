b05dd7a13123477a2d8f15e2adfd8ecdaf962f37
RF <rf@eggnine.com>
2024-08-17 01:30:59 -0400

    Added new xunit project, updated license

---
+++ TrashTaf.XUnit/UnitTest1.cs
@@ -0,0 +1,12 @@
+// TrashTaf © 2024 by RF@EggNine.com All Rights Reserved
+namespace TrashTaf.XUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
