da90bf33c356368301654f2b2c941389f7539eda
RF <rf@eggnine.com>
2024-08-21 00:40:26 -0400

    Added SkipIfBrowserIsNot, moved tests to tests folder, moved configuration tests to separate class, implemented #20

---
+++ Eggnine.TrashTaf.XUnit/SkipAttributes/SkipIfBrowserIsNot.cs
@@ -0,0 +1,22 @@
+﻿namespace Eggnine.TrashTaf.XUnit.SkipAttributes
+{
+    public class SkipIfBrowserIsNot : SkipIf
+    {
+        public SkipIfBrowserIsNot(string browser)
+        {
+            Browser = browser;
+        }
+
+        public string Browser { get; }
+
+        public override bool Matches(TrashContext ctx)
+        {
+            return string.Equals(Browser, ctx.BrowserName);
+        }
+
+        public override string Reason(TrashContext ctx)
+        {
+            return $"BrowserName is {ctx.BrowserName} and not {Browser}";
+        }
+    }
+}
