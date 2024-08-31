fcdee26d7f3babda8dfebc85dc3a76291ccee807
rf-eggnine <164746698+rf-eggnine@users.noreply.github.com>
2024-08-31 17:36:31 -0400

    Update init.sql -add columns
    Signed-off-by: rf-eggnine <164746698+rf-eggnine@users.noreply.github.com>

--- init.sql
+++ init.sql
@@ -1,7 +1,15 @@
 CREATE TABLE TestRuns (
     TestRunID INT PRIMARY KEY,
-    TestName VARCHAR(255) NOT NULL,
-    Status VARCHAR(50),
-    StartTime DATETIME,
-    EndTime DATETIME
+    testName VARCHAR(255),
+    className VARCHAR(255),
+    runDateTime TIMESTAMP,
+    durationMs INTEGER,
+    result VARCHAR(50),
+    exceptionType VARCHAR(255),
+    exceptionMessage VARCHAR(255),
+    operatingSystemName VARCHAR(25),
+    operatingSystemVersion VARCHAR(25),
+    browserName VARCHAR(25),
+    browserVersion VARCHAR(25),
+    logMessages TEXT
 );
